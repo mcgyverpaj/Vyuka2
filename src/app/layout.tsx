74a9b3f1be1853483f23d781a74a15092303d51a
mcgyverpaj <mcgyverpaj@gmail.com>
2025-05-02 08:38:03 +0200

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,12 @@
-// src/app/layout.tsx
-
 import type { Metadata } from 'next'
 import { ReactNode } from 'react'
 
+// Pokud NEMÁŠ soubor ./globals.css, tento řádek SMAŽ
+// import './globals.css'
+
 export const metadata: Metadata = {
-  title: 'OCR Notes App',
-  description: 'Aplikace pro převod zápisků na text',
+  title: 'OCR App',
+  description: 'OCR aplikace pro převod obrázků na text',
 }
 
 export default function RootLayout({ children }: { children: ReactNode }) {
